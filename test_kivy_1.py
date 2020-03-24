c623897d8669f9d17709bcd1147c4bfd3991f4a6
Joseph Pancho <jnpancho120@yahoo.com>
2020-03-23 19:23:01 -0500

    Initial commit

---
+++ test_kivy_1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/kivy
+import kivy
+from kivy.properties import StringProperty
+
+kivy.require('1.7.2')
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+Builder.load_string("""
+<Highest>:
+    GridLayout:
+        cols: 1
+        Button:
+            text: root.r1c2
+            on_press: root.increment()
+        # Button:
+        #     text: root.r1c2
+        #     on_press: root.increment()
+""")
+
+class Highest(Screen):
+    count = 0
+    r1c2 = StringProperty(str("Currency: "+ str(count)))
+
+    def increment(self):
+        self.count = self.count + 1
+
+        self.r1c2 = str("Currency: " + str(self.count))
+
+
+# Create the screen manager
+sm = ScreenManager()
+sm.add_widget(Highest(name='Highest'))
+
+class TestApp(App):
+
+    def build(self):
+        return sm
+
+if __name__ == '__main__':
+    TestApp().run()
