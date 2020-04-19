84bcdc2df6ffd1662603c3b80ace02a608cd8935
2020jschultz <42749630+2020jschultz@users.noreply.github.com>
2020-04-19 18:24:46 -0500

    Update savegameprogress.py

--- savegameprogress.py
+++ savegameprogress.py
@@ -2,12 +2,4 @@ import shelve
 
 shelfFile = shelve.open('saved_game_filename')
 
-mainBoard = shelfFile ['mainBoardVariable']
-
-playerTile = shelfFile ['playerTileVariable']
-
-computerTile = shelfFile ['computerTileVariable']
-
-showHints = shelfFile ['showHintsVariable']
-
 shelfFile.close()
